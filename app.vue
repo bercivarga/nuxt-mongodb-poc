b4846c42a573fc878f79b48c6b7e0c6b267d84ec
bercivarga <65171545+bercivarga@users.noreply.github.com>
2024-04-30 19:15:47 +0200

    add base tailwind styles and global setup

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ useSeoMeta({
 
 <template>
   <SeoHead />
-  <div>
-    <h1 class="text-lg text-red-500">Hello world!</h1>
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-soft bg-white rounded-lg p-8">
+    <h1 class="text-3xl font-bold text-red-500">Hello world!</h1>
   </div>
 </template>
