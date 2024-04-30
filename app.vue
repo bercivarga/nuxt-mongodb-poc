077e405aec5324991a3f9737d666933b1c00029c
bercivarga <65171545+bercivarga@users.noreply.github.com>
2024-04-30 18:58:22 +0200

    initialise project and add base seo tags

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Fullstack Nuxt.js Application with TypeScript and MongoDB',
+  description: 'Fullstack Nuxt.js Application with TypeScript and MongoDB using Vite as the build tool.',
+})
+</script>
+
+<template>
+  <SeoHead />
+  <div>
+    <h1>Hello world!</h1>
+  </div>
+</template>
