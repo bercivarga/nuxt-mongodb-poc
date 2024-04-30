99029c096989d805c2e4e09c4acb729cdb6718d3
bercivarga <65171545+bercivarga@users.noreply.github.com>
2024-04-30 19:40:01 +0200

    initial formatting tests and adjustments

--- components/seo/head.vue
+++ components/seo/head.vue
@@ -1,3 +1,6 @@
+<!-- This is the only place where disabling eslint is needed. -->
+<!-- eslint-disable vue/html-self-closing -->
+
 <template>
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,27 @@ export default {
   theme: {
     colors: {
       black: {
-        DEFAULT: "#2B2C37"
+        DEFAULT: "#2B2C37",
       },
       gray: {
-        DEFAULT: "#E1DFDF"
+        DEFAULT: "#E1DFDF",
       },
       white: {
-        DEFAULT: "#FFFFFF"
-      }
+        DEFAULT: "#FFFFFF",
+      },
     },
     extend: {
       boxShadow: {
-        "soft": "0 4px 40px 0 rgba(43, 44, 55, 0.1)"
+        soft: "0 4px 40px 0 rgba(43, 44, 55, 0.1)",
       },
       // Note that Helvetica Neue isn't a browser base font and isn't available on Google Fonts,
       // therefore I assume that you are testing on a Mac or have Helvetica Neue installed on your system.
       // Otherwise the font falls back to another sans-serif font.
       fontFamily: {
-        "sans": ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
-        "body": ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
-      }
+        sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+        body: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
