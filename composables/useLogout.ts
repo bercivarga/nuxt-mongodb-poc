7ba9e35cc81dc792a1e570382e68562e4202c372
bercivarga <65171545+bercivarga@users.noreply.github.com>
2024-05-11 21:52:38 +0200

    Clean up composables

--- composables/useLogout.ts
+++ composables/useLogout.ts
@@ -1,16 +1,16 @@
 import Cookies from "js-cookie";
 import type { AuthStatus } from "./shared.types";
 
-const status = ref<AuthStatus>("idle");
-
-async function logout() {
-  status.value = "loading";
-  Cookies.remove("token");
-  status.value = "idle";
-  await navigateTo("/");
-}
-
 export default function useLogout() {
+  const status = ref<AuthStatus>("idle");
+
+  async function logout() {
+    status.value = "loading";
+    Cookies.remove("token");
+    status.value = "idle";
+    await navigateTo("/");
+  }
+
   return {
     status,
     logout,
